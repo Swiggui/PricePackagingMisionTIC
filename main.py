3866c25e285810b750b4719a6ca46c2a14f0a99f
Figui Camilo <99374843+Swiggui@users.noreply.github.com>
2022-04-30 17:12:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Introducir datos
+alto = float(input())
+ancho = float(input())
+profundo = float(input())
+#calcula los datos introducidos
+volumen = alto * ancho * profundo
+coste = volumen * 5
+#imprime datos calculados
+print(volumen)
+print(coste)
