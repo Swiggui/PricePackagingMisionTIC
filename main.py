b2c81cd0f034ea9b5693130a73059e5530422106
Juan Camilo Figueredo Montaña (Figui) <99374843+Swiggui@users.noreply.github.com>
2022-05-05 21:18:00 +0000

    Se resuelve con respecto a los requerimentos: Con respecto a la situación planteada, usa el código de la semana pasada y agrega los elementos necesarios. Usa un condicional IF que haga que se agregue 2000 al costo si la altura es mayor a 30 cm. Usa otro condicional IF que haga que se agregue el IVA del costo si el costo es mayor a 10000.
    Finalmente, imprime el VOLUMEN del paquete y el COSTO del envío, EN ESE ORDEN.
    
    IMPORTANTE: Cuando vayas a entregar el reto, quítale todos los textos decorativos o informativos para el usuario. Solo debes dejar el código funcional. Es decir, si imprimes cosas tipo “Ingrese …”, “El valor es …”, deberás de quitarlo. Solo imprime lo que te indica el planteamiento del problema y en el orden en que se solicita.

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ profundo = float(input())
 #calcula los datos introducidos
 volumen = alto * ancho * profundo
 coste = volumen * 5
-
+#Validar si la altura es mayor a 30 y suma 2k al coste
+if alto > 30:
+  coste = coste + 2000
+#Verifica si el coste es mayor a 10k y le agrega el IVA
+if coste > 10000:
+    coste = coste + (coste * 0.19)
 #imprime datos calculados
 print(volumen)
 print(coste)
