524ad38f653d3845ed896924b5fbdb09f9a58a48
Juan Camilo Figueredo Montaña (Figui) <99374843+Swiggui@users.noreply.github.com>
2022-05-09 00:10:36 +0000

    Se habilita la posibilidad de calcular más de un solo paquete y calcula el coste total

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-#Introducir datos
-alto = float(input())
-ancho = float(input())
-profundo = float(input())
-#calcula los datos introducidos
-volumen = alto * ancho * profundo
-coste = volumen * 5
-#Validar si la altura es mayor a 30 y suma 2k al coste
-if alto > 30:
-  coste = coste + 2000
-#Verifica si el coste es mayor a 10k y le agrega el IVA
-if coste > 10000:
+#inserte el numero de paquetes a calcular
+totalCost = 0
+numPack = int(input())
+for i in range(numPack):
+  #Introducir datos
+  alto = float(input())
+  ancho = float(input())
+  profundo = float(input())
+  #calcula los datos introducidos
+  volumen = alto * ancho * profundo
+  coste = volumen * 5
+  #Validar si la altura es mayor a 30 y suma 2k al coste
+  if alto > 30:
+    coste = coste + 2000
+  #Verifica si el coste es mayor a 10k y le agrega el IVA
+  if coste > 10000:
     coste = coste + (coste * 0.19)
-#imprime datos calculados
-print(volumen)
-print(coste)
+  #imprime datos calculados
+  totalCost = totalCost + coste
+  print(volumen)
+  print(coste)
+
+print(totalCost)
